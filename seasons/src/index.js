57836e8bf2b2f081f7279a02997bbb99b03bd8df
Amir Behzad <a.behzad.ebr@gmail.com>
2019-09-02 22:02:09 +0430

    handles errors

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,19 +6,27 @@ class App extends React.Component {
     super(props);
     // THIS IS THE INLY TIME we do direct assignment
     // to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
